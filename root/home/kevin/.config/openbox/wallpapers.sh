420c9b462432aea8d7569bccd409f5034858f77d
lujianmei <anysky130@163.com>
2015-05-17 15:00:06 +0800

    first init

---
+++ root/home/kevin/.config/openbox/wallpapers.sh
@@ -0,0 +1,10 @@
+#!/bin/env bash
+WALLPAPERS="/home/kevin/.icons"
+ALIST=( `ls -w1 $WALLPAPERS` )
+RANGE=${#ALIST[@]}
+let "number = $RANDOM"
+let LASTNUM="`cat $WALLPAPERS/.last` + $number"
+let "number = $LASTNUM % $RANGE"
+echo $number > $WALLPAPERS/.last
+
+feh --bg-scale $WALLPAPERS/${ALIST[$number]}
